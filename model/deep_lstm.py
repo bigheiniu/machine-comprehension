4d13a575fc30139cff0ec7044acc073cfbd901ae
yichuan <yichuan1@asu.edu>
2018-10-16 17:44:08 -0700

    user information

--- model/deep_lstm.py
+++ model/deep_lstm.py
@@ -2,72 +2,84 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.contrib import rnn
 from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.nn.rnn_cell import LSTMCell
 
-from .cell import LSTMCell, MultiLSTMCell
+# from .cell import LSTMCell, MultiLSTMCell
 
 
 class DeepLSTM:
-    def __init__(self, config):
+    def __init__(self, config,datainfo):
         self.config = config
+        self.datainfo = datainfo
         self.add_placeholders()
         self.pred, self.loss, self.train_op = self.build_graph()
 
     def add_placeholders(self):
-        self.input_placeholder = tf.placeholder(tf.int32, [None, None])
-        self.labels_placeholder = tf.placeholder(tf.int32, [None])
+        #assume every question and answer have the same length
+        #handle one question and all the answer at the same time
+        self.question_placeholder = tf.placeholder(tf.int32,[self.config.maxlen, 1])
+        #ATTENTION: best answer in the first place
+        self.answer_placeholder = tf.placeholder(tf.int32,[self.config.maxlen, None])
+        self.score_placeholder = tf.placeholder(tf.int32, [None])
         self.sequence_length = tf.placeholder(tf.int32, [None])
         self.state_keep_prob = tf.placeholder(tf.float32, shape=())
 
-    def create_feed_dict(self, inputs_batch, sequence_length, state_keep_prob=1.0, labels_batch=None):
+    def create_feed_dict(self, answer,question,sequence_length, state_keep_prob=1.0, score=None):
         feed_dict = {
-            self.input_placeholder: inputs_batch,
-            self.state_keep_prob: state_keep_prob,
+            self.answer_placeholder: answer,
+            self.question_placeholder:question ,
             self.sequence_length: sequence_length
         }
         if labels_batch is not None:
-            feed_dict[self.labels_placeholder] = labels_batch
+            feed_dict[self.score_placeholder] = score
         return feed_dict
 
     def generate_cell(self):
-        return rnn.DropoutWrapper(rnn.LSTMCell(self.config.state_size, True), output_keep_prob=self.state_keep_prob)
+        # use ordinary cell
+        return rnn.DropoutWrapper(LSTMCell(self.config.numhidden), output_keep_prob=self.state_keep_prob)
 
     #        return rnn.DropoutWrapper(LSTMCell(self.config.state_size), state_keep_prob=self.state_keep_prob)
-    def user_defined_cell(self):
-        return rnn.DropoutWrapper(LSTMCell(self.config.state_size), state_keep_prob=self.state_keep_prob)
+    # def user_defined_cell(self):
+    #     return rnn.DropoutWrapper(LSTMCell(self.config.state_size), state_keep_prob=self.state_keep_prob)
 
     def build_graph(self):
+        #TODO: set to pre-trained word2vect
+        
         embed = tf.get_variable(
             "word_embedding", shape=[self.config.vocab_size, self.config.embed_size], initializer=xavier_initializer()
         )
-        x = tf.nn.embedding_lookup(embed, self.input_placeholder)
+
+
+        question = tf.nn.embedding_lookup(embed, self.question_placeholder)
+        answer = tf.nn.embedding_lookup(embed,self.answer_placeholder)
+        #depth should be one
         cells = [self.generate_cell() for _ in range(self.config.depth)]
         stack_cells = rnn.MultiRNNCell(cells)
-        _, state = tf.nn.dynamic_rnn(stack_cells, x, sequence_length=self.sequence_length, dtype=tf.float32)
-        h = tf.concat([it[0] for it in state], axis=1, name="h")
+        with tf.variable_scope('train'):
+            _, state_quesiton = tf.nn.dynamic_rnn(stack_cells, question, sequence_length=self.sequence_length, dtype=tf.float32)
+
+        #PUZZLE: what will happen if there batch of answer 
+        with tf.variable_scope('train',reuse=True):
+            _, state_answer = tf.nn.dynamic_rnn(stack_cells, answer, sequence_length=self.sequence_length, dtype=tf.float32)
+        
+        answer_vector = tf.concat([it[0] for it in state_answer], axis=1, name="h")
+        question_vector = tf.concat([it[0] for it in state_question], axis=1, name="h")
+        
+        #init User Variable
+        #TODO: use user profile to initialize user vector
+        user = tf.get_variable("user",[self.datainfo.userSize, self.config.userDim])
+        Loss = []
+        goodAns = df.gather(answer_vector,0)
+        goodUser = df.gather(answer_vector, 0)
+        # x: count of All answer
+        for i in range(1, x):
+            a1 = tf.gather(answer_vector,0)
+            
         w_0 = tf.get_variable("w_0", [self.config.vocab_size, self.config.state_size * self.config.depth],
                               initializer=xavier_initializer())
-        """
-        stack_cells = self.generate_cell()  # MultiLSTMCell(cells)
-        _, state = tf.nn.dynamic_rnn(stack_cells, x, sequence_length=self.sequence_length, dtype=tf.float32)
-        self.h, _ = state
-        w_0 = tf.get_variable("w_0", [self.config.vocab_size, self.config.state_size],
-                              initializer=xavier_initializer())
-        """
+
         b_0 = tf.get_variable("b_0", [self.config.vocab_size], dtype=tf.float32, initializer=tf.zeros_initializer())
         pred = tf.matmul(h, w_0, transpose_b=True) + b_0
-
-        """
-        nce_loss = tf.nn.nce_loss(
-            weights=w_0,
-            biases=b_0,
-            labels=tf.expand_dims(self.labels_placeholder, 1),
-            inputs=h,
-            num_sampled=self.config.num_sampled,
-            num_classes=self.config.vocab_size,
-            partition_strategy="div",
-            name="nce_loss"
-        )
-        """
 
         loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=pred,
